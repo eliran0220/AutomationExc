b1d51261238f9691b58a4303ac79e1cd7dec8097
eliran0220 <eliran0220@gmail.com>
2020-09-22 00:59:45 +0300

    Update item-list.component.ts

--- frontend/item-managment-app/src/app/item-list/item-list.component.ts
+++ frontend/item-managment-app/src/app/item-list/item-list.component.ts
@@ -30,14 +30,20 @@ export class ItemListComponent implements OnInit {
      }
    })
   }
-
+  
+  /**
+  * the open function is used to open whatever operation the user has chosen to.
+  */
   open(content) {
     this.modal.open(content, {ariaLabelledBy: 'modal-basic-content'}).result.then((result) => {
       ;
     }, (reason) => {
     });
   }
-
+  
+  /**
+  * This activating the get_item function which returns an item by id.
+  */
   onGetSubmit(form:NgForm):any {
     var id = form.value.Id;
     this.itemService.get_item(id).subscribe((data:any) =>{
@@ -59,7 +65,9 @@ export class ItemListComponent implements OnInit {
 }
 
   
-
+  /**
+  * This activates the deposit_quantity function which deposits an amount to a given id.
+  */
   onDepositSubmit(form:NgForm, item_id) :any {
     this.modal.dismissAll();
     var amount = form.value.Amount;
@@ -78,7 +86,10 @@ export class ItemListComponent implements OnInit {
     );
   
   }
-
+  
+  /**
+  * This activates the withdraw_quantity function which withdraws an amount to the chosen item.
+  */
   onWithdrawSubmit(form:NgForm, item_id) :any {
     this.modal.dismissAll();
     var amount = form.value.Amount;
@@ -97,6 +108,9 @@ export class ItemListComponent implements OnInit {
     );
   }
 
+  /**
+  * This activates the delete_item function which deletes the chosen item.
+  */
   onDeleteSubmit(template:NgbModal,item_id) :any {
     this.modal.dismissAll();
     this.itemService.delete_item(item_id).subscribe((data:any) => {
@@ -114,6 +128,9 @@ export class ItemListComponent implements OnInit {
     );
   }
 
+  /**
+  * This activates the add_item function which adds the given item to the database.
+  */
   onAddSubmit(form:NgForm):any{
     this.modal.dismissAll();
     this.itemService.add_item(form.value.Name,form.value.Amount,form.value.Description).subscribe((data:any)=>{
@@ -134,6 +151,9 @@ export class ItemListComponent implements OnInit {
     )      
   }
 
+  /**
+  * This activates the update_item function which updates the chosen item via the given fields..
+  */
   onUpdateSubmit(form:NgForm,item_id) :any{
     this.modal.dismissAll();
     var name = form.value.Name;
@@ -154,6 +174,9 @@ export class ItemListComponent implements OnInit {
     );
   }
 
+  /**
+  * This section is helper functions which changes the state of the item given the operation.
+  */
   updateItem(id,name,amount,description){
     for (var i = 0; i<this.item_list.length; i++){
       if (this.item_list[i].id === id){
